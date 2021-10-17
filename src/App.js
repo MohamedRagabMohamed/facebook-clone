f2534d0020a68a65a919abe3f2d4ebfa28ce0f69
mohamed ragab <mohamed225.mr@gmail.com>
2021-10-17 02:38:59 +0200

    Project base
    Its's an a empty react project to build in it.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>lets build the facebook clone</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCuOIXzwEvthSxi-zcOXcdLqqpasUTRR2o",
+  authDomain: "facebook-clone-3bdb7.firebaseapp.com",
+  projectId: "facebook-clone-3bdb7",
+  storageBucket: "facebook-clone-3bdb7.appspot.com",
+  messagingSenderId: "725335482607",
+  appId: "1:725335482607:web:acf3f9825bdc3172d5bb1a",
+  measurementId: "G-SXL247T40B",
+};
